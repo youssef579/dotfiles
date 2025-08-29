489e1ced8e49c43254457e0f1aad24e289da4fc4
youssefragaey <youssefragaa3@gmail.com>
2025-08-29 20:38:11 +0300

    Add atuin, paperwm, ulauncher

---
+++ .config/paperwm/user.js
@@ -0,0 +1,12 @@
+// -*- mode: gnome-shell -*-
+const Extension = imports.misc.extensionUtils.getCurrentExtension();
+const Tiling = Extension.imports.tiling;
+const Keybindings = Extension.imports.keybindings;
+
+function enable() {
+    // Runs when extension is enabled
+}
+
+function disable() {
+    // Runs when extension is disabled
+}
