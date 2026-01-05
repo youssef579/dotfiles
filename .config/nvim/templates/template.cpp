4ef35128ec928150af171f779a4a5af0babbe38b
youssefragaey <youssefragaa3@gmail.com>
2026-01-05 14:31:05 +0200

    Postinstall fedora

--- .config/nvim/templates/template.cpp
+++ .config/nvim/templates/template.cpp
@@ -3,9 +3,20 @@
 // URL: $(URL)
 
 #include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
 using namespace std;
 using ll = long long;
 using ld = long double;
+using namespace __gnu_pbds;
+template <class T>
+using ordered_set = tree<T, null_type, std::less<T>, rb_tree_tag,
+                         tree_order_statistics_node_update>;
+template <class T>
+using ordered_multiset = tree<T, null_type, std::less_equal<T>, rb_tree_tag,
+                              tree_order_statistics_node_update>;
+
 #define endl '\n'
 #define MOD 1000000007
 #define ALL(v) (v).begin(), (v).end()
