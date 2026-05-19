d316bcf60655ba1aebef725fcb2db04556267a4e
Youssef Ragaey <youssefragaa3@gmail.com>
2026-05-19 03:02:04 +0300

    Change some settings

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -168,6 +168,7 @@ vim.o.tabstop = 2
 vim.o.shiftwidth = 2
 vim.o.expandtab = true
 vim.o.softtabstop = 2
+vim.g.editorconfig = true
 
 vim.opt.fillchars = 'eob: ' -- Remove the ~'s
 vim.o.wrap = false -- Disable line wrap
